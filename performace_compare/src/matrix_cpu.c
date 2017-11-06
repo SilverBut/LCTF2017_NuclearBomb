215c1c051d9be779f0914e5f4aa2f02db3b56624
SilverBut <SilverBut@users.noreply.github.com>
2017-11-06 15:26:30 +0800

    Add initial code

---
+++ performace_compare/src/matrix_cpu.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void){
+	printf("--- CPU Performance Test ---\n");
+	return 0;
+}
---
+++ performace_compare/src/matrix_gpu.cu
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(void){
+	std::cout << "--- GPU Performance Test ---" << std::endl;;
+	return 0;
+}
